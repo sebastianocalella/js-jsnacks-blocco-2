a7cfbf561d72dbec0939a871bb5aa4a2c4e19a43
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-25 15:53:14 +0100

    odd selector

--- odd numbers array/script.js
+++ odd numbers array/script.js
@@ -1,1 +1,9 @@
-array = [];
+numbers = [];
+
+for (let i=0; i<6; i++){
+    number = parseInt(prompt('inserisci un numero'));
+    if (number % 2 == 1){
+        numbers.push(number);
+    }
+}
+console.log(numbers);
