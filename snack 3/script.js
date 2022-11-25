17263c5f8820c41a3ad8678d6d7321b66e2aec75
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-25 17:43:31 +0100

    better maxvalue feature

--- snack 3/script.js
+++ snack 3/script.js
@@ -1,20 +1,20 @@
 const list = [
-    12,
-    2,
-    4,
-    5,
-    65,
-    34,
-    9,
-    19,
-    0,
-    12,
-    4,
-    9
+    -12,
+    -2,
+    -4,
+    -5,
+    -65,
+    -34,
+    -9,
+    -19,
+    -5,
+    -12,
+    -4,
+    -9
 ]
 
 let somma = 0;
-let maxValue = 0;
+let maxValue = list[0];
 
 for (let i=0; i<10; i++){
     somma += list[i];
