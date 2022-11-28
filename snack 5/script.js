111267a37d34ea636755a61c53907bf3356dbf9c
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-28 11:11:56 +0100

    space between name and surname

--- snack 5/script.js
+++ snack 5/script.js
@@ -6,6 +6,6 @@ const faulseList = [];
 n = parseInt(prompt('inserisi il numero di invitati falsi da aggiungere alla lista:'));
 
 for(let i=0; i<n; i++){
-    faulseList.push(names[Math.floor(Math.random()*7)] + surnames[Math.floor(Math.random()*7)]);
+    faulseList.push(names[Math.floor(Math.random()*7)] + " " + surnames[Math.floor(Math.random()*7)]);
 }
 console.log(faulseList);
