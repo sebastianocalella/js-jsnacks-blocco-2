bd4e58a0b30a31516ae64b804e12c4964d54efab
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-28 12:40:48 +0100

    only odd list numbers

---
+++ snack 6 switch/script.js
@@ -0,0 +1,28 @@
+const numberList = [
+    2,
+    456,
+    7,
+    54,
+    32,
+    5,
+    4,
+    32,
+    43,
+    5,
+    452,
+    2,
+    3,
+    65,
+    86,
+    2
+];
+
+for(let i = 0; i<numberList.length; i++){
+    switch (i%2){
+        case 0:
+            console.log(numberList[i]);
+            break;
+        case 1:
+            break;
+    }
+}
